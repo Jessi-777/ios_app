0995150912017c730892fb8805ed11d47a772786
Jessi Chavez Gallego <angelwerkengineeringmail.com>
2022-11-19 20:00:08 -0800

    sign up bold

---
+++ screens/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import Text from "@kaloraat/react-native-text";
+
+const Signup = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+            <Text bold center color="#333">Sign Up</Text>
+        
+        </View>
+    )
+}
+
+export default Signup;
